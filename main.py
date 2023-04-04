74faad4d6ed7c7230bab804ab0c532b97f8fee3b
Vladimir Dorofeev <daskodex@gmail.com>
2023-04-04 19:25:01 +0600

    refactoring match_case construction

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 import random
 
-GIGABYTE = 1024*1024*1024
-MEGABYTE = 1024*1024
+GIGABYTE = 1024 * 1024 * 1024
+MEGABYTE = 1024 * 1024
 KILOBYTE = 1024
 
 
@@ -13,52 +13,37 @@ def timer(func):
         runtime = time.perf_counter() - start
         print(f'\n{func.__name__} took {runtime:.4f} secs')
         return result
+
     return _wrapper
 
-@timer
-def random_dict_insert(length):
-    rand_dict = {}
-    for i in range(length):
-        rand_dict[i] = i
-        print(f'\r step dict: {i}/{length}', end='')
-
-    else:
-        return True
 
 @timer
-def random_list_append(length):
-    rand_list = []
-    for i in range(length):
-        rand_list.append(i)
-        print(f'\r step list random insert: {i}/{length}', end='')
+def ranadom_data_insert_test(datatype, length):
+    match datatype:
+        case 'dict':
+            rand_dict = {}
+            for i in range(length):
+                rand_dict[i] = i
+                print(f'\r step dict: {i}/{length}', end='')
+        case 'listappend':
+            rand_list = []
+            for i in range(length):
+                rand_list.append(i)
+                print(f'\r step list random insert: {i}/{length}', end='')
+        case 'listinsert':
+            rand_list = []
+            for i in range(length):
+                rand_list.insert(random.randint(0, i), i)
+                print(f'\r step list append: {i}/{length}', end='')
+        case 'set':
+            rand_set = set()
+            for i in range(length):
+                rand_set.add(i)
+                print(f'\r step list set insert: {i}/{length}', end='')
 
-    else:
-        return True
-
-@timer
-def random_list_insert(length):
-    rand_list = []
-    for i in range(length):
-        rand_list.insert(random.randint(0, i), i)
-        print(f'\r step list append: {i}/{length}', end='')
-
-    else:
-        return True
-
-
-@timer
-def random_set_append(length):
-    rand_set = set()
-    for i in range(length):
-        rand_set.add(i)
-        print(f'\r step list set insert: {i}/{length}', end='')
-
-    else:
-        return True
 
 if __name__ == '__main__':
-    random_dict_insert(MEGABYTE)
-    random_set_append(MEGABYTE)
-    random_list_append(MEGABYTE)
-    random_list_insert(MEGABYTE)
-
+    ranadom_data_insert_test('dict', MEGABYTE)
+    ranadom_data_insert_test('set', MEGABYTE)
+    ranadom_data_insert_test('listappend', MEGABYTE)
+    ranadom_data_insert_test('listinsert', MEGABYTE)
